403d19dea87f1fec9befec30a78ac64244e0483e
walianitin <walianitin828@gmail.com>
2025-10-23 21:44:45 +0530

    gallery component

--- src/app/Gallery/page.tsx
+++ src/app/Gallery/page.tsx
@@ -1,10 +1,14 @@
-"use client"
-import Spline from '@splinetool/react-spline/next';
-export default function Fonts() {
-  return (
-    <div className=' max-h-fit max-w-fit'>
-            <Spline scene="https://prod.spline.design/6kyy9BgTnw-rrT6t/scene.splinecode"> </Spline>
-            <div>asds</div>
+
+export default function Gallery(){
+
+    return <div className="flex flex-col h-screen p-3 gap-2" style={{
+        backgroundImage:"url(/stars.svg)" }}>
+                <div className="min-h-3/4 min-w-full">
+                                        above purple part
+                </div>
+                <div className="  min-h-px w-screen bg-gradient-to-r from-transparent via-sky-600 to-transparent "></div>
+                <div className=" min-h-1/4 min-w-full ">
+                        the bellow purple part
+                </div>
     </div>
-  );
 }
