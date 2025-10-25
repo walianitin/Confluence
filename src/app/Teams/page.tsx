e333d451845b9ceead03b004f093bb1658c57ea5
walianitin <walianitin828@gmail.com>
2025-10-25 23:53:29 +0530

    nitn

--- src/app/Teams/page.tsx
+++ src/app/Teams/page.tsx
@@ -1,11 +1,17 @@
 
-import Card from "../components/Card"
-export default function Teams(){
-        return  <div>
-                    {/* <Circle></Circle> */}
-                    <Card>
-
-                    </Card>
+export default function Teams() {
+  return (
+    <div>
+      <div className=" min-w-screen h-screen bg-white flex flex-col ">
+        <div className="flex flex-row items-center justify-center h-1/3 ">
+          <div
+            className=" w-[600px] h-[600px] rounded-full 
+              bg-[radial-gradient(circle_at_center,_rgba(139,92,246,0.9)_40%,_rgba(1,1,1,0.2)_70%)] blur-xs opacity-70 text-2xl">
+            <div>Teams</div>
+          </div>
         </div>
-
+        <div className=" flex-1 bg-amber-600 drop-shadow-amber-200">sadf</div>
+      </div>
+    </div>
+  );
 }
--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -16,14 +16,11 @@ const navbarContent=[{
 },{
     name:"Gallery",
     link:"/Gallery"
-},{
-    name:"events",
-    link:"/events"
-},]
+}]
 export default function Navbar(){
         const router=useRouter();
     return <>
-        <div className="bg-transparent  flex flex-row  text-shadow-white text-xl font-extralight cursor-pointer ">
+        <div className="bg-transparent  flex flex-row  w-full h-20  text-shadow-white text-xl font-extralight cursor-pointer ">
             <div className='min-w-1/3 flex flex-row items-center justify-center pl-30' onClick={() => router.push('/')}>
                 <Image src={"/conflu25White.png"} width={250} height={200} alt={"Asdf"} />            </div>
                     <div className="flex flex-row min-w-2/3 gap-5  justify-end items-center pr-20 ">
